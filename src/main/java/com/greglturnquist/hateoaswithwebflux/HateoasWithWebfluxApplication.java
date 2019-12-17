876b3610c182e123ea46ea9dbc0d1b3a193690ec
Greg Turnquist <gturnquist@pivotal.io>
2019-12-17 16:27:58 -0600

    Initial commit.
    Demonstrates https://github.com/spring-projects/spring-boot/issues/19392

---
+++ src/main/java/com/greglturnquist/hateoaswithwebflux/HateoasWithWebfluxApplication.java
@@ -0,0 +1,12 @@
+package com.greglturnquist.hateoaswithwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasWithWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasWithWebfluxApplication.class, args);
+	}
+}
---
+++ src/main/java/com/greglturnquist/hateoaswithwebflux/HomeController.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.greglturnquist.hateoaswithwebflux;
+
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.*;
+
+import reactor.core.publisher.Mono;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Greg Turnquist
+ */
+@RestController
+public class HomeController {
+
+	@GetMapping
+	Mono<? extends RepresentationModel<?>> index() {
+
+		Mono<Link> selfLink = linkTo(methodOn(HomeController.class).index()).withSelfRel().toMono();
+
+		return selfLink //
+			.map(link -> {
+				RepresentationModel<?> model = new RepresentationModel<>();
+				model.add(link);
+				return model;
+			});
+	}
+}
